13c137f8644a3aeefc01cb80383abc1ec895eb22
Derek Middlebrook <derekmiddlebrook@Dereks-Mac-Studio-3.local>
2026-03-20 18:24:30 -0700

    Add Google Tag Manager (GTM-TJSL4TKS)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -44,6 +44,14 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src= 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f); })(window,document,'script','dataLayer','GTM-TJSL4TKS');`,
+          }}
+        />
+        {/* End Google Tag Manager */}
+        
         <link rel="icon" href="/favicon.ico" sizes="any" />
         <link rel="icon" href="/icon.svg" type="image/svg+xml" />
         <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
@@ -84,6 +92,13 @@ export default function RootLayout({
         />
       </head>
       <body className="antialiased bg-white text-slate-800">
+        {/* Google Tag Manager (noscript) */}
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TJSL4TKS" height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+          }}
+        />
+        {/* End Google Tag Manager (noscript) */}
         {children}
       </body>
     </html>
